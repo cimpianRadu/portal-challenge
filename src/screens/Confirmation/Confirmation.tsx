e050745ee57818a6ad521ab9badec8b92724e568
cimpianRadu <cimpian.radu@yahoo.com>
2023-12-19 20:21:45 +0200

    control polling

--- src/screens/Confirmation/Confirmation.tsx
+++ src/screens/Confirmation/Confirmation.tsx
@@ -6,6 +6,7 @@ import {Text, View} from 'react-native';
 
 const Confirmation = ({route}: {route: ConfirmationScreenRouteProp}) => {
   const {transactionHash} = route.params;
+  const stopPolling = React.useRef(false);
 
   const {isLoading, data} = useGetTransactionStatusQuery(
     {
@@ -13,8 +14,18 @@ const Confirmation = ({route}: {route: ConfirmationScreenRouteProp}) => {
     },
     {
       pollingInterval: 5000,
+      skip: stopPolling.current,
     },
   );
+
+  React.useEffect(() => {
+    if (!isLoading) {
+      stopPolling.current = true;
+    }
+    return () => {
+      stopPolling.current = true;
+    };
+  }, []);
 
   if (isLoading) {
     return <LoadingIndicator />;
@@ -27,6 +38,20 @@ const Confirmation = ({route}: {route: ConfirmationScreenRouteProp}) => {
       {data.status === 'success' && <Text style={{fontSize: 50}}>✅</Text>}
       {data.status === 'error' && <Text>❌</Text>}
       <Text>Transaction status {data.status}</Text>
+
+      <Text>
+        <Text style={{fontSize: 16, color: 'gray'}}>TX hash: </Text>
+        {data.txHash}
+      </Text>
+      <View style={{height: 20}} />
+      <Text>
+        <Text style={{fontSize: 16, color: 'gray'}}>
+          Successfully sent to:{' '}
+        </Text>
+        {data.receiver}
+      </Text>
+      <View style={{height: 12}} />
+      <Text>{data.value}</Text>
     </View>
   );
 };
